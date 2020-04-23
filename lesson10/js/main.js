8b2b7921a27744eed61832605a4f7ecc5e06a780
ArtemMachulenko <dr.a.mefesto@gmail.com>
2020-04-23 13:35:29 +0300

    -complete task 2

--- lesson10/js/main.js
+++ lesson10/js/main.js
@@ -92,10 +92,12 @@
                 return;
             }
 
-            const tax = .5;
-            const moneyAfterTax = money - (money/100 * tax);
+            this.takeCredits(money); //снять деньги с карты 1
+            //отнять налог за перевод
+            const TRANSFER_TAX = .5;
+            const moneyAfterTax = money - (money/100 * TRANSFER_TAX);
 
-            receiverUser.putCredits(moneyAfterTax);
+            receiverUser.putCredits(moneyAfterTax); //пополнить карту 2
 
             //log
             const log = {
@@ -121,7 +123,7 @@
     //получить информацию по карте
     //console.log(card1.getCardOptions()); //{balance: 100, transactionLimit: 100, historyLogs: Array(0), key: 1}
 
-    //положить деньги
+    //пополнить карту
     card1.putCredits(1000);
     //console.log(card1.getCardOptions()); //{balance: 1100, transactionLimit: 100, historyLogs: Array(0), key: 1}
 
@@ -142,18 +144,111 @@
     //card1.transferCredits(5050, card2); //Лимит снятия денег перевышен!
     //card1.transferCredits(2050, card2); //Недостаточно денег на счету!
 
+    //баланс карты 1 до пересыла
+    //console.log(card1.getCardOptions()); //{balance: 1000, transactionLimit: 5000, historyLogs: Array(0), key: 1}
+
     card1.transferCredits(150, card2);
     card1.transferCredits(50, card2);
     card1.transferCredits(250, card2);
+
+    //баланс карты 1 после пересыла
+    //console.log(card1.getCardOptions()); // {balance: 550, transactionLimit: 5000, historyLogs: Array(3), key: 1}
+    //баланс карты 2 после пересыла
     //console.log(card2.getCardOptions()); //{balance: 547.75, transactionLimit: 100, historyLogs: Array(0), key: 2}
 
     //посмотреть историю переводов
-    console.log(card1.getCardOptions().historyLogs);
+    // console.log(card1.getCardOptions().historyLogs);
     //[{operationType: "Reseived credits", credits: 150, operationTime: "4/23/2020, 11:17:01"},
     // {operationType: "Reseived credits", credits: 50, operationTime: "4/23/2020, 11:17:01"},
     // {operationType: "Reseived credits", credits: 250, operationTime: "4/23/2020, 11:17:01"}
     // ]
-    
+
     //====================================================================================================
+    // 2) Створити UserAccount:
+    // Створити класс `UserAccount` (для цього завдання можна використати ES6 класс
+    // або звичайну функцію):
+    // const user = new UserAccount('Bob');
+    // Класс має містити:
+    // Властивості акаунту користувача:
+    // - name (передати в конструкторі)
+    // - cards (Масив карток користувача. Масив не має бути довшим ніж 3)
+    // Методи акаунту користувача:
+    // - addCard. Створює нову карту (використовуйте ‘userCard’ функцію) та додайте її до
+    // cards:
+    //     user.addCard();
+    // Користувач повинен мати <= 3 карти.
+    // - getCardByKey. Приймає число в діапазоні {1; 3} і повертає об’єкт карти
+    // /*
+    // * Повертає об’єкт {
+    // * key: 1,
+    // * balance: 150,
+    // * ...other card properties
+    // * }
+    // */
+    // user.getCardByKey(1);
+    // Код
+    // Приклад переказу коштів:
+    // let user = new UserAccount('Bob');
+    // user.addCard()
+    // user.addCard()
+    // let card1 = user.getCardByKey(1);
+    // let card2 = user.getCardByKey(2);
+    // card1.putCredits(500);
+    // card1.setTransactionLimit(800);
+    // card1.transferCredits(300, card2);
+    // card2.takeCredits(50);
+    // console.log(card1.getCardOptions()); // див зображення 2
+
+    class UserAccount {
+        constructor(name) {
+            this.name = name;
+            this.cards = [];
+        }
+
+        addCard() {
+            if (this.cards.length > 3) return;
+            const key = this.cards.length + 1;
+            const card = userCard(key);
+            this.cards.push(card);
+        }
+
+        getCardByKey(key) {
+            if (key > 3 && key <= 0) return;
+
+            return this.cards.find(card => card.getCardOptions().key === key);
+        }
+    }
+
+    const user = new UserAccount('Bob');
+
+    //добавить карты
+    user.addCard();
+    user.addCard();
+
+    //получить карту по ключу
+    const userCard1 = user.getCardByKey(1);
+    const userCard2 = user.getCardByKey(2);
+
+    //положить деньги на карту 1
+    userCard1.putCredits(500);
+    //console.log(userCard1.getCardOptions()); //{balance: 600, transactionLimit: 100, historyLogs: Array(0), key: 1}
+
+    //установить лимит на карту 1
+    userCard1.setTransactionLimit(800);
+    //console.log(userCard1.getCardOptions()); //{balance: 600, transactionLimit: 800, historyLogs: Array(0), key: 1}
+
+    //перевести деньги с карты 1 на карту 2
+    userCard1.transferCredits(300, userCard2);
+    //console.log(userCard1.getCardOptions()); //{balance: 300, transactionLimit: 800, historyLogs: Array(1), key: 1}
+    //console.log(userCard2.getCardOptions()); //{balance: 398.5, transactionLimit: 100, historyLogs: Array(0), key: 2}
+
+    //снять деньги с карты 2
+    userCard2.takeCredits(50);
+    //console.log(userCard2.getCardOptions()); //{balance: 348.5, transactionLimit: 100, historyLogs: Array(0), key: 2}
+
+    //показать карту 1
+    //console.log(userCard1.getCardOptions()); //{balance: 300, transactionLimit: 800, historyLogs: Array(1), key: 1}
+    //log
+    //console.log(userCard1.getCardOptions().historyLogs); //[{operationType: "Reseived credits", credits: 300, operationTime: "4/23/2020, 13:21:19"}]
 }
 
